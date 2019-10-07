4cbbdd181686c0797c2becfc539c1d08bff5a0eb
Gabriel Abrantes <gabrantes010@gmail.com>
2019-10-07 17:57:30 -0300

    Builder-dp

---
+++ builder-dp/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		RobotBuilder oldStyleRobotBuilder = new OldRobotBuilder();
+		RobotEngineer robotEngineer = new RobotEngineer(oldStyleRobotBuilder);
+		
+		robotEngineer.makeRobot();
+		
+		Robot firstRobot = robotEngineer.getRobot();
+		
+		System.out.println("Robot Built");
+		
+		System.out.println("Robot Head Type: " + firstRobot.getRobotHead());
+		System.out.println("Robot Torso Type: " + firstRobot.getRobotTorso());
+		System.out.println("Robot Arm Type: " + firstRobot.getRobotArms());
+		System.out.println("Robot Leg Type: " + firstRobot.getRobotLegs());
+	}
+}
